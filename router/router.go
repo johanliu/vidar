489f8fb30951a31786f5bfe7dc342a0e48d89259
Yuan Liu (刘远) <liuyuan@le.com>
2016-06-15 17:01:47 +0800

    Modify router.go

--- router/router.go
+++ router/router.go
@@ -20,16 +20,48 @@ func New() *Router {
 	return &Router{handlers: make(map[string][]*Endpoint)}
 }
 
-func (r *Router) Handler(method string, path string, h http.Handler) {
+func (r *Router) Add(method string, path string, h http.Handler) {
+
 	if path[0] != '/' {
 		logger.Error.Fatalf("Path must begin with '/' but in : %s", path)
 	}
 
-	r.Add(method, path, h)
+	r.handlers[path] = append(r.handlers[path], &Endpoint{method: method, Handler: h})
 }
 
-func (r *Router) Add(method string, path string, h http.Handler) {
-	r.handlers[path] = append(r.handlers[path], &Endpoint{method: method, Handler: h})
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *Request) {
+
+	if eds, ok := r.handlers[req.URL.Path]; ok {
+		for _, ed := range eds {
+			if ed.method == req.Method {
+				ed.Handler.ServeHTTP(w, req)
+				return
+			}
+		}
+
+		logger.Info.Printf("%s:%s Method Not Allowed", req.Method, req.URL.Path)
+		HTTP.Error((w, "Method Not Allowed", 405)
+
+	} else {
+		logger.Info.Printf("%s Not Found", req.URL.Path)
+		HTTP.Error(w, "URL Not Found", 404)
+	}
+}
+
+func (r *Router) GET(path string, h http.Handler) {
+	r.Add("GET", path, h)
+}
+
+func (r *Router) POST(path string, h http.Handler) {
+	r.Add("POST", path, h)
+}
+
+func (r *Router) DELETE(path string, h http.Handler) {
+	r.Add("DELETE", path, h)
+}
+
+func (r *Router) PUT(path string, h http.Handler) {
+	r.Add("PUT", path, h)
 }
 
 // TODO: add regex or tree-based path resolution
